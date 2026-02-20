11a298157c2e9e6c4f169e5483efac6e849ba03b
Kundan Patil <ikundanpatil@gmail.com>
2026-02-20 21:56:44 +0530

    Add files via upload

---
+++ BrandNameGenerator.py
@@ -0,0 +1,4 @@
+print("Welcome To The Brand Name Generator")
+fname = input("What Is Your First Name: \n")
+lname = input("What Is Your Last Name: \n")
+print("Your Brand Name Is: " + fname + " " + lname)
